3192b47c52e23ef317707a3b5e4e02eb71aeb443
Jonathan Lalande <lalandej.gg@gmail.com>
2020-06-28 17:31:04 -0400

    particle system with birth rate

--- src/ParticleSystem.hs
+++ src/ParticleSystem.hs
@@ -6,20 +6,29 @@ module ParticleSystem
 ) where
 
 import Graphics.Gloss
-import Particle (Particle)
+import Particle (Particle, newParticle, updateParticle, drawParticle)
 
 data ParticleSystem = ParticleSystem { position :: Point
                                      , elapsed :: Float
+                                     , birthRate :: Float
                                      , particles :: [Particle] }
 
 newParticleSystem :: ParticleSystem
 newParticleSystem = ParticleSystem { position = (0, -200)
                                    , elapsed = 0
+                                   , birthRate = 1
                                    , particles = [] }
 
 updateParticleSystem :: Float -> ParticleSystem -> ParticleSystem
-updateParticleSystem ms ps = ps { elapsed = (elapsed ps) + ms }
+updateParticleSystem ms ps = ps { elapsed = elapsed'
+                                , particles = particles' }
+  where elapsed' = (elapsed ps) + ms
+        elapsedSecond = fromIntegral $ (floor elapsed') - (floor $ elapsed ps)
+        particlesToCreate = (birthRate ps) * elapsedSecond
+        particles' = map (updateParticle ms) (particles ps) ++ (take (floor particlesToCreate) $ repeat newParticle)
 
 drawParticleSystem :: ParticleSystem -> Picture
-drawParticleSystem ps = translate x y $ circle 16
+drawParticleSystem ps = Pictures $ [origin] ++ particlesPictures
   where (x, y) = position ps
+        origin = translate x y $ circle 8
+        particlesPictures = drawParticle <$> (particles ps)
